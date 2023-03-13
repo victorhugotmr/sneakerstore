d8be1eb084f36f7486cdbdc5d5e350b73da5064d
Victor Hugo <torugoestudos@gmail.com>
2023-03-13 17:09:16 -0300

    data dos sneakers

---
+++ src/data/sneakersData.js
@@ -0,0 +1,322 @@
+export const sneakerData = [
+  {
+    id: "01",
+    brand: "Nike",
+    model: "SB Dunk Low Shanghai (2004)",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F052%2F787%2F516%2Foriginal%2F23459_00.png.png&w=640&q=75",
+    release: {
+      date: "2004-01-01",
+      year: "2004",
+    },
+    price: "$65",
+    colorway: {
+      main: "White",
+      details: "Metallic/Gold-Redwood",
+    },
+  },
+  {
+    id: "02",
+    brand: "Nike",
+    model: "Air Force 1 Low A Cold Wall Black",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F079%2F018%2F367%2Foriginal%2F468805_00.png.png&w=640&q=75",
+    release: {
+      date: "2018-12-21",
+      year: "2018",
+    },
+    price: "$190",
+    colorway: {
+      main: "Black",
+      details: "White-Dark/Grey",
+    },
+  },
+  {
+    id: "03",
+    brand: "Nike",
+    model: "Air Force 1 Elite White Ice",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimages.stockx.com%2Fimages%2FNike-Air-Force-1-Elite-White-Ice.png%3Ffit%3Dfill%26bg%3DFFFFFF%26w%3D500%26h%3D500%26fm%3Dwebp%26auto%3Dcompress%26trim%3Dcolor%26q%3D90%26dpr%3D2%26updated_at%3D1622097481&w=640&q=75",
+    release: {
+      date: "2016-02-01",
+      year: "2016",
+    },
+    price: "$130",
+    colorway: {
+      main: "White",
+      details: "White",
+    },
+  },
+  {
+    id: "04",
+    brand: "Nike",
+    model: "Air Max 90 Firecracker",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimages.stockx.com%2Fimages%2FNike-Air-Max-90-Firecracker.jpg%3Ffit%3Dfill%26bg%3DFFFFFF%26w%3D500%26h%3D500%26fm%3Dwebp%26auto%3Dcompress%26trim%3Dcolor%26q%3D90%26dpr%3D2%26updated_at%3D1660656225&w=640&q=75",
+    release: {
+      date: "2022-03-17",
+      year: "2022",
+    },
+    price: "$130",
+    colorway: {
+      main: "White",
+      details: "Black/Bright Crimson/Laser Blue",
+    },
+  },
+  {
+    id: "05",
+    brand: "Nike",
+    model: "Free RN Motion Flyknit 2018 Oreo",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F069%2F036%2F072%2Foriginal%2F477212_00.png.png&w=640&q=75",
+    release: {
+      date: "2018-01-01",
+      year: "2018",
+    },
+    price: "$170",
+    colorway: {
+      main: "Black",
+      details: "White",
+    },
+  },
+  {
+    id: "06",
+    brand: "adidas",
+    model: "Stan Smith Core Black White",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F019%2F786%2F610%2Foriginal%2FBD7452.png.png&w=640&q=75",
+    release: {
+      date: "2019-03-07",
+      year: "2019",
+    },
+    price: "$80",
+    colorway: {
+      main: "Core Black",
+      details: "Footwear White/Core Black",
+    },
+  },
+  {
+    id: "07",
+    brand: "adidas",
+    model: "Advantage White Green (Junior)",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F036%2F499%2F872%2Foriginal%2FEF0213.png.png&w=640&q=75",
+    release: {
+      date: "-",
+      year: "-",
+    },
+    price: "$50",
+    colorway: {
+      main: "Cloud White",
+      details: "Green/Grey Two",
+    },
+  },
+  {
+    id: "08",
+    brand: "adidas",
+    model: "Stan Smith Marvel Hulk",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F067%2F906%2F236%2Foriginal%2F802363_00.png.png&w=640&q=75",
+    release: {
+      date: "2021-08-09",
+      year: "2021",
+    },
+    price: "$100",
+    colorway: {
+      main: "Core Black",
+      details: "Silver Metallic/Scarlet",
+    },
+  },
+  {
+    id: "09",
+    brand: "adidas",
+    model: "Yeezy Foam RNNR Ararat",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F082%2F889%2F714%2Foriginal%2F651749_00.png.png&w=640&q=75",
+    release: {
+      date: "2020-06-26",
+      year: "2020",
+    },
+    price: "$75",
+    colorway: {
+      main: "White",
+      details: "White-White",
+    },
+  },
+  {
+    id: "10",
+    brand: "adidas",
+    model: "Yeezy Boost 350 V2 Citrin (Non-Reflective)",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F079%2F327%2F463%2Foriginal%2F529364_00.png.png&w=640&q=75",
+    release: {
+      date: "2019-09-23",
+      year: "2019",
+    },
+    price: "$200",
+    colorway: {
+      main: "Citrin",
+      details: "Citrin/Citrin",
+    },
+  },
+  {
+    id: "11",
+    brand: "Puma",
+    model: "Suede Pepsi Black",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F016%2F119%2F877%2Foriginal%2F366332_02.png.png&w=640&q=75",
+    release: {
+      date: "2018-10-25",
+      year: "2018",
+    },
+    price: "$110",
+    colorway: {
+      main: "Puma Black",
+      details: "Silver",
+    },
+  },
+  {
+    id: "12",
+    brand: "Puma",
+    model: "RS-X Atmos Amazon Green",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimages.stockx.com%2Fimages%2FPuma-RS-X-Atmos-Amazon-Green.png%3Ffit%3Dfill%26bg%3DFFFFFF%26w%3D500%26h%3D500%26fm%3Dwebp%26auto%3Dcompress%26trim%3Dcolor%26q%3D90%26dpr%3D2%26updated_at%3D1626898632&w=640&q=75",
+    release: {
+      date: "2019-12-01",
+      year: "2019",
+    },
+    price: "$130",
+    colorway: {
+      main: "Amazon Green",
+      details: "",
+    },
+  },
+  {
+    id: "13",
+    brand: "Puma",
+    model: "Thunder Rive Droite Glacier Grey Barbados Cherry",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F015%2F238%2F925%2Foriginal%2F369452_02.png.png&w=640&q=75",
+    release: {
+      date: "2018-10-01",
+      year: "2018",
+    },
+    price: "$120",
+    colorway: {
+      main: "Glacier Gray",
+      details: "Barbados Cherry",
+    },
+  },
+  {
+    id: "14",
+    brand: "Puma",
+    model: "Clyde Public Enemy",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F035%2F214%2F123%2Foriginal%2F374539_01.png.png&w=640&q=75",
+    release: {
+      date: "2020-02-14",
+      year: "2020",
+    },
+    price: "$90",
+    colorway: {
+      main: "High Risk Red",
+      details: "Puma Black",
+    },
+  },
+  {
+    id: "15",
+    brand: "Puma",
+    model: "Alteration Kurve Black Limelight",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F023%2F706%2F927%2Foriginal%2F369794_02.png.png&w=640&q=75",
+    release: {
+      date: "2019-07-03",
+      year: "2019",
+    },
+    price: "$155",
+    colorway: {
+      main: "Black",
+      details: "Limelight",
+    },
+  },
+  {
+    id: "16",
+    brand: "MSCHF",
+    model: "Big Red Boot",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F084%2F562%2F149%2Foriginal%2FMSCHF010.png&w=640&q=75",
+    release: {
+      date: "2023-02-16",
+      year: "2023",
+    },
+    price: "$350",
+    colorway: {
+      main: "Red",
+      details: "",
+    },
+  },
+  {
+    id: "17",
+    brand: "MSCHF",
+    model: "Gobstomper 'Sour Edition'",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F082%2F454%2F928%2Foriginal%2F1090640_00.png.png&w=640&q=75",
+    release: {
+      date: "2022-12-06",
+      year: "2022",
+    },
+    price: "$195",
+    colorway: {
+      main: "Purple",
+      details: "Green/Lime",
+    },
+  },
+  {
+    id: "18",
+    brand: "MSCHF",
+    model: "AC.1 'Grey'",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F082%2F412%2F354%2Foriginal%2F1075156_00.png.png&w=640&q=75",
+    release: {
+      date: "2022-10-19",
+      year: "2022",
+    },
+    price: "$450",
+    colorway: {
+      main: "Grey",
+      details: "",
+    },
+  },
+  {
+    id: "19",
+    brand: "MSCHF",
+    model: "Jimmy Fallon x MSCHF Gobstomper",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F076%2F259%2F201%2Foriginal%2F1008107_00.png.png&w=640&q=75",
+    release: {
+      date: "2022-07-28",
+      year: "2022",
+    },
+    price: "$195",
+    colorway: {
+      main: "Off White",
+      details: "Red/Yellow/Blue",
+    },
+  },
+  {
+    id: "20",
+    brand: "MSCHF",
+    model: "Super Normal 'Coke White'",
+    imageURL:
+      "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F074%2F881%2F435%2Foriginal%2F996048_00.png.png&w=640&q=75",
+    release: {
+      date: "2022-06-23",
+      year: "2022",
+    },
+    price: "$145",
+    colorway: {
+      main: "Coke White",
+      details: "",
+    },
+  },
+];
