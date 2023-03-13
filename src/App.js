d2247b683cff9080b6ed41c40f3b849c81b7e133
Victor Hugo <torugoestudos@gmail.com>
2023-03-13 17:11:18 -0300

    arquivos react

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { Grid, ThemeProvider } from "@mui/material";
+
+import theme from "./theme/Theme";
+
+// importacao dos componentes
+// de forma desestruturada 
+// dentro do arquivo index.js
+import {
+  Header,
+  Content,
+  Spacing,
+  Footer,
+  SearchSneaker,
+  MapContainer,
+  FormContainer,
+} from "./components";
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid container direction="column">
+        <Grid item>
+          <Header />
+          <Content />
+          <SearchSneaker />
+          <Spacing />
+          <MapContainer />
+          <Spacing />
+          <FormContainer />
+          <Footer />
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
+
+export default App;
