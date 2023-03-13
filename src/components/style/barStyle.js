154b77538de0d9687773e6e44972c94908bfc2f9
Victor Hugo <torugoestudos@gmail.com>
2023-03-13 17:02:40 -0300

    componentes de estilo

---
+++ src/components/style/barStyle.js
@@ -0,0 +1,10 @@
+
+// criacao e exportacao da funcao 
+// estiliza o elemento
+export const barStyle = {
+  display: "flex",
+  justifyContent: "center",
+  borderRadius: "40px",
+  width: "100%",
+  height: 60
+};
