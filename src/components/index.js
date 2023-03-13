3ff23a666db9a5c794ea12adc9ec2b117e16f0bb
Victor Hugo <torugoestudos@gmail.com>
2023-03-13 17:08:54 -0300

    exportacao dos arquivos

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import Header from "./layout/header/Header"
+import Content from "./layout/content/Content"
+import Footer from "./layout/footer/Footer"
+import Spacing from "./layout/spacing/Spacing"
+import SearchSneaker from "./containers/sneaker/searchSneaker"
+import MapContainer from "./containers/map/mapContainer"
+import FormContainer from "./containers/form/FormContainer"
+
+export { Header, Content, Footer, Spacing, SearchSneaker, MapContainer, FormContainer }
+
+// importa o e exportacao dos 
+// componentes em um arquivo unico
