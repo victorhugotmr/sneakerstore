eb903a25dd8064710d7378d5ab316c51349f599a
Victor Hugo <torugoestudos@gmail.com>
2023-03-22 23:34:36 -0300

    update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -5,8 +5,9 @@ import Spacing from "./layout/spacing/Spacing"
 import SearchSneaker from "./containers/sneaker/searchSneaker"
 import MapContainer from "./containers/map/mapContainer"
 import FormContainer from "./containers/form/FormContainer"
+import Drag from "./containers/drag/Drag"
 
-export { Header, Content, Footer, Spacing, SearchSneaker, MapContainer, FormContainer }
+export { Header, Content, Footer, Spacing, SearchSneaker, MapContainer, FormContainer, Drag }
 
 // importa o e exportacao dos 
 // componentes em um arquivo unico
