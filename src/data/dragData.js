9cf72936302aeaaaa8a73e79d05c4a0d38663774
Victor Hugo <torugoestudos@gmail.com>
2023-03-22 23:37:54 -0300

    drag and drop data

---
+++ src/data/dragData.js
@@ -0,0 +1,64 @@
+const dragData = [
+  {
+    id: 1,
+    title: "Jordan 1 Low Taxi",
+    src: "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F076%2F972%2F522%2Foriginal%2F1007017_00.png.png&w=640&q=75",
+  },
+  {
+    id: 2,
+    title: "Nike Dunk Low PRM Halloween",
+    src: "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F061%2F953%2F917%2Foriginal%2FDM0088_100.png.png&w=640&q=75",
+  },
+  {
+    id: 3,
+    title: "Converse Chuck Taylor All-Star 70 Hi",
+    src: "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F024%2F785%2F105%2Foriginal%2F164912C.png.png&w=640&q=75",
+  },
+  {
+    id: 4,
+    title: "Converse Chuck Taylor All-Star Hi",
+    src: "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F073%2F839%2F832%2Foriginal%2F774831_00.png.png&w=640&q=75",
+  },
+  {
+    id: 5,
+    title: "Comme des Garçons x Era Pro",
+    src: "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F067%2F178%2F103%2Foriginal%2FVN_OVFB9QS.png.png&w=640&q=75",
+  },
+  {
+    id: 6,
+    title: "Comme des Garçons HOMME",
+    src: "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F042%2F677%2F270%2Foriginal%2FCDG_997S_BLK.png.png&w=640&q=75",
+  },
+  {
+    id: 7,
+    title: "Golf Le Fleur x One Star Ox 'Grinch'",
+    src: "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F018%2F158%2F291%2Foriginal%2FX31417_OS128U_18F01.png.png&w=640&q=75",
+  },
+  {
+    id: 8,
+    title: "Converse Gianno Golf Le Fleur",
+    src: "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F083%2F157%2F834%2Foriginal%2F621257_00.png.png&w=640&q=75",
+  },
+  {
+    id: 9,
+    title: "Vans Old Skool Primary Check",
+    src: "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F022%2F641%2F813%2Foriginal%2FVN0A38HBP0S.png.png&w=640&q=75",
+  },
+  {
+    id: 10,
+    title: "Vans Vault OG Old Skool LX",
+    src: "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F065%2F891%2F225%2Foriginal%2FVN0A4P3X7AG.png.png&w=640&q=75",
+  },
+  {
+    id: 11,
+    title: "Fila Grant Hill 1 Bred 'Red & Black'",
+    src: "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F039%2F385%2F185%2Foriginal%2F1BM00638_014.png.png&w=640&q=75",
+  },
+  {
+    id: 12,
+    title: "Wmns Filaray 'White'",
+    src: "https://thesneakerdatabase.com/_next/image?url=https%3A%2F%2Fimage.goat.com%2Fattachments%2Fproduct_template_pictures%2Fimages%2F016%2F895%2F293%2Foriginal%2F5RM00522_125.png.png&w=640&q=75",
+  },
+];
+
+export default dragData
